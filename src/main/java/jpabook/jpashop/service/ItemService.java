28fee1e0241f5e39bcbd1dbae533857fc53adc1d
SL313 <rebin0313@gmail.com>
2023-01-03 18:16:59 +0900

    item Repository & Service

---
+++ src/main/java/jpabook/jpashop/service/ItemService.java
@@ -0,0 +1,30 @@
+package jpabook.jpashop.service;
+
+import jpabook.jpashop.domain.Item.Item;
+import jpabook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+
+    public List<Item> findAll(){
+        return itemRepository.findAll();
+    }
+}
