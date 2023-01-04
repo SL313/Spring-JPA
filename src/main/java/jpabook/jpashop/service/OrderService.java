44f6c6b22b0d2b03c0cc721f0ec34912939b46c8
SL313 <rebin0313@gmail.com>
2023-01-04 18:19:38 +0900

    order methods

---
+++ src/main/java/jpabook/jpashop/service/OrderService.java
@@ -0,0 +1,23 @@
+package jpabook.jpashop.service;
+
+public class OrderService {
+
+    /**
+     * order()
+     * 엔티티 조회
+     * 배송정보 생성
+     * 주문상품 생성
+     * 주문 생성
+     * 주문 저장
+     * */
+
+    /**
+     * cancel()
+     * 주문 엔티티 조회
+     * 주문 취소
+     * */
+
+
+
+
+}
